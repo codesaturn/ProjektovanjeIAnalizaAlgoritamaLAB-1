aa8326af84753dde2ce76564fe098931b3e5a2b1
andrijacenicelfak <andrija.cenic@elfak.rs>
2022-03-15 19:15:55 +0100

    SoundEx primeri i analiza

--- SoundEx/soundex.cpp
+++ SoundEx/soundex.cpp
@@ -17,7 +17,9 @@ using std::chrono::system_clock;
 char transform(char c) {
     if (c == '\0')
         return c;
+
     char nc = '\0';
+
     switch (c) {
         case 'b':
         case 'f':
@@ -64,9 +66,11 @@ void soundex(char* str) {
             str[j] = str[j + 1];
 
     for (int i = 1; i < strlen(str); i++) {
-        if (transform(str[i]) == '\0' && str[i] >= 'a')
+        if (transform(str[i]) == '\0' && str[i] >= 'a') {
             for (int j = i; j < strlen(str); j++)
                 str[j] = str[j + 1];
+            i--;
+        }
     }
 
     if (strlen(str) < 4) {
@@ -77,7 +81,42 @@ void soundex(char* str) {
 }
 
 int main() {
-    char zaba[100] = "Rubin";
-    soundex(zaba);
-    cout << zaba;
+    char fileName[100];
+    char str[100];
+    char strf[100];
+    char nosx[100];
+
+    cout << "Unesi naziv fajla : " << endl;
+    cin.getline(fileName, 100);
+
+    cout << "Unesi string za pretrazivanje : " << endl;
+    cin.getline(str, 100);
+
+    ifstream f(fileName);
+    ofstream rez("rez.txt", ios::app);
+
+    rez << "File : " << fileName << endl;
+    rez << "Str : " << str << endl;
+    rez << "Duzina : " << strlen(str) << endl;
+    soundex(str);
+    rez << "SoundEx : " << str << endl;
+    rez << "Rezultati : " << endl;
+    int i = 1;
+    auto millsStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    while (!f.eof()) {
+        f >> strf;
+        strcpy(nosx, strf);
+        soundex(strf);
+        if (strcmp(str, strf) == 0) {
+            rez << "\t";
+            rez << i << ", " << nosx << endl;
+        }
+        i++;
+    }
+    auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - millsStart;
+    rez << "Time : " << end << " ms\n";
+    rez << "---------------------------------\n";
+
+    rez.close();
+    f.close();
 }
